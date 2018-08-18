a1926fc6a8527598d611a9428b78ad0e3653017e
Himanshu Sagar <hsagarthegr8@gmail.com>
2018-08-18 15:33:09 +0530

    Integrated CourseListComponent

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import{CoursesService} from '../courses.service'
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.css']
 })
 export class CourseListComponent implements OnInit {
-
-  constructor() { }
+  
+    courses:any
+  constructor(private service:CoursesService) { }
 
   ngOnInit() {
-  }
+this.displayCourses();  
+}
+displayCourses() {
+      this.service.displayCourses()
+        .subscribe(data => this.courses = data);
+}
 
 }
